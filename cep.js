a7dadc286ffc89bde689704e5e639c4a2dec5e38
Textil-Sauter <156688139+Textil-Sauter@users.noreply.github.com>
2024-01-22 12:01:43 -0300

    Update cep.js

--- cep.js
+++ cep.js
@@ -25,21 +25,3 @@ function popularForm(resposta) {
     localidade.value = resposta.localidade;
     estado.value = resposta.uf;
 }
-
-let cepEntrega = document.querySelector('#cep_entrega');
-let logradouroEntrega = document.querySelector('#endereco_entrega');
-
-cep.addEventListener('blur', function(e) {
-    let cep = e.target.value;
-    let script = document.createElement('script');
-    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
-    document.body.appendChild(script);
-});
-
-function popularForm(resposta) {
-    if("erro" in resposta){
-        alert('CEP não encontrado');
-        return;
-    }
-    logradouroEntrega.value = resposta.logradouroEntrega;    
-}
